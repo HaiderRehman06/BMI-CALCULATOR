2885cb5042d828db32edb6237e2b36d73fa843f9
Haider Rehman <haiderrehman500@gmail.com>
2024-10-11 23:08:32 +0500

    adding animation to the wrapper div

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ button.addEventListener('click', () => {
 
 
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
+        
         if(bmi < 18.6){
             result.innerHTML = 'Under weight : ' + bmi;
         }else if(bmi >= 18.6 && bmi < 24.9){
